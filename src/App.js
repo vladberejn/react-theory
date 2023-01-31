d0a9164a46cf603efbdc0fdfc57996b7b27689c4
vladberejn <vladberejn@gmail.com>
2023-01-31 19:59:29 +0300

    Block-3

--- src/App.js
+++ src/App.js
@@ -3,19 +3,75 @@ import './App.css';
 import Car from './Car/Car'
 
 class App extends Component {
+    state = {
+        cars: [
+            {name: 'Ford', year: 2018},
+            {name: 'Audi', year: 2016},
+            {name: 'Mazda', year: 2010},
+        ],
+        pageTitle: 'React components',
+        showCars: false,
+    }
+
+    onChangeName (name, index) {
+        const car = this.state.cars[index]
+        car.name = name
+        const cars = [...this.state.cars]
+        car[index] = car // doing nothing, make be commented
+        this.setState({cars})
+    }
+
+    toggleCarsHandler = () => {
+
+        this.setState({ // setState позволяет вносить изменения в state
+            showCars: !this.state.showCars
+        })
+    }
+
+    deleteHandler(index) {
+        const cars = this.state.cars.concat()
+        cars.splice(index, 1)
+        this.setState({cars})
+    }
   render() {
     const divStyle = {textAlign: 'center'}
+    let cars = null
+    if (this.state.showCars){
+        cars = this.state.cars.map((car, index)=>{
+            return (
+                <Car
+                    key={index}
+                    name={car.name} // пропы, которые я передаю в КАР.ЖС и их значения
+                    year={car.year}
+                    onDelete = {this.deleteHandler.bind(this, index)}
+                    onChangeName={event=>this.onChangeName(event.target.value, index)}
+                />
+            )
+        })
+    }
+
     return (
       <div style={divStyle} className="App">
-        <h1>Hello World!</h1>
+        <h1>{this.state.pageTitle}</h1>
 
-        <Car name={'Ford'} year={2018}>
-            <p style={{color:'blue'}}>COLOR</p>
-        </Car>
-        <Car name="Audi" year={2016}>
-            <p style={{color:'red'}}>COLOR</p>
-        </Car>
-        <Car name="Mazda" year={2010}/>
+        <button onClick={this.toggleCarsHandler}>Toggle cars</button>
+
+          { cars }
+        {/*<Car*/}
+        {/*    name={cars[0].name}*/}
+        {/*    year={cars[0].yaer}*/}
+        {/*    onChangeTitle ={this.changeTitleHandler.bind(this, cars[0].name)}*/}
+        {/*/>*/}
+        {/*<Car*/}
+        {/*    name={cars[1].name}*/}
+        {/*    year={cars[1].yaer}*/}
+        {/*    onChangeTitle={()=>this.changeTitleHandler(cars[1].name)}*/}
+        {/*/>*/}
+        {/*<Car*/}
+        {/*    name={cars[2].name}*/}
+        {/*    year={cars[2].yaer}*/}
+        {/*    onChangeTitle={()=>this.changeTitleHandler(cars[2].name)}*/}
+        {/*/>*/}
       </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -8,9 +8,16 @@ import React from "react";
 // const Car = () => <h2>This is car component</h2>
 
 export default props => (
-    <div>
+    <div style={{
+        border: '1px solid #ccc',
+        marginBottom: '10px',
+        display: "block",
+        padding: '10px'
+    }}>
+        {/*Возвращаю хтмл код с полученными из Апп пропами и значениями */}
         <h3>Car name: {props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
-        {props.children}
+        <input type="text" name="" id="" onChange={props.onChangeName} value={props.name}/>
+        <button onClick={props.onDelete}>Delete</button>
     </div>
 )
